67c6cab5bd7a88a8536b4af9b1ceb17a7d3248e2
Matheus Santos <141796283+matsilvaa@users.noreply.github.com>
2025-05-06 10:43:02 -0300

    Update testeetet.py

--- testeetet.py
+++ testeetet.py
@@ -1,4 +1,21 @@
-adasda
-asd
-asda
+lista = [12, 7, 34, 3, 25, 18, 45, 22, 9, 16, 1, 39, 6, 27, 4, 11, 14, 30, 20, 8] # Lista Principal
+lista_ordenada = sorted(lista)
+
+alvo = 45
+
+lista_ordenada.append(alvo)
+lista_ordenada = sorted(lista_ordenada) #Ordenando a lista com o alvo
+
+print(lista_ordenada)
+
+vistos = set()
+for numero in lista_ordenada:
+    complemento = alvo - numero
+
+    if complemento in vistos:
+        print("Os valores são:")
+        print(complemento)
+        print(numero)
+    vistos.add(numero)
+
 
